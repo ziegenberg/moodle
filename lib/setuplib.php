2497753b663d53c4f8b146ec4a8d75dc8ebd1bea
Daniel Ziegenberg <daniel@ziegenberg.at>
2021-06-11 21:23:50 +0200

    MDL-71897 setuplib: add haproxy as recognized server
    Instead of handing off the requests from HAProxy to a webserver
    (e.g. Apache or Nginx) and then via FastCGI to PHP-FPM, the requests
    are directly relayed from HAProxy to a running PHP-FPM service.
    This enables directly load-balancing a moodle cluster running on
    PHP-FPM using HAProxy and the new FastCGI backend in HAProxy 2.1
    reducing overhead and separating static content from scripts thus
    enabling independent scaling.
    
    Signed-off-by: Daniel Ziegenberg <daniel@ziegenberg.at>

--- lib/setuplib.php
+++ lib/setuplib.php
@@ -1058,6 +1058,10 @@ function setup_get_remote_url() {
         //LiteSpeed - not officially supported
         $rurl['fullpath'] = $_SERVER['REQUEST_URI']; // TODO: verify this is always properly encoded
 
+    } else if (stripos($_SERVER['SERVER_SOFTWARE'], 'HAProxy') !== false) {
+        // HAproxy - not officially supported.
+        $rurl['fullpath'] = $_SERVER['REQUEST_URI']; // TODO: verify this is always properly encoded.
+
     } else if ($_SERVER['SERVER_SOFTWARE'] === 'HTTPD') {
         //obscure name found on some servers - this is definitely not supported
         $rurl['fullpath'] = $_SERVER['REQUEST_URI']; // TODO: verify this is always properly encoded
