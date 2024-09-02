c3419ab6aeed31c502a952edfea232512eefe9ea
Daniel Ziegenberg <daniel@ziegenberg.at>
2024-09-02 23:07:56 +0200

    MDL-31071 blocks: replace deprecated file_encode_url
    Signed-off-by: Daniel Ziegenberg <daniel@ziegenberg.at>

--- blocks/private_files/renderer.php
+++ blocks/private_files/renderer.php
@@ -78,8 +78,16 @@ class block_private_files_renderer extends plugin_renderer_base {
             }
         }
         foreach ($dir['files'] as $file) {
-            $url = file_encode_url("$CFG->wwwroot/pluginfile.php", '/'.$tree->context->id.'/user/private'.$file->get_filepath().$file->get_filename(), true);
             $filename = $file->get_filename();
+            $url = \core\url::make_pluginfile_url(
+                $tree->context->id,
+                'user',
+                'private',
+                null,
+                $file->get_filepath(),
+                $filename,
+                true
+            )->out();
             $image = $this->output->pix_icon(file_file_icon($file), '');
             $result .= '<li role="treeitem">'.html_writer::link($url, $image.$filename, ['tabindex' => -1]).'</li>';
         }
