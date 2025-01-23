0a7f67bd8b028e515217d4c6ed2f3743be4b27bb
Daniel Ziegenberg <daniel@ziegenberg.at>
2025-01-23 16:33:23 +0100

    MDL-84308 portfolio_core: Remove portfolio_mahara from Moodle 5.0
    Signed-off-by: Daniel Ziegenberg <daniel@ziegenberg.at>

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -1382,5 +1382,14 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2025011700.02);
     }
 
+    // Remove portfolio_mahara.
+    if ($oldversion < 2025012400.01) {
+        if (!file_exists($CFG->dirroot . "/portfolio/mahara/version.php")) {
+            uninstall_plugin('portfolio', 'mahara');
+        }
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2025012400.01);
+    }
+
     return true;
 }
