1bbdf3d5e8111a8fcc8e0f46355e264d12652ef5
Daniel Ziegenberg <daniel@ziegenberg.at>
2025-02-04 10:32:28 +0100

    MDL-84160 libraries: Add league/uri
    Signed-off-by: Daniel Ziegenberg <daniel@ziegenberg.at>

--- lib/classes/component.php
+++ lib/classes/component.php
@@ -129,6 +129,7 @@ class component {
         \Invoker::class => 'lib/php-di/invoker/src',
         \JmesPath::class => 'lib/jmespath/src',
         \Kevinrob\GuzzleCache::class => 'lib/guzzlehttp/kevinrob/guzzlecache/src',
+        \League\Uri::class => 'lib/league/uri/src',
         \Laravel\SerializableClosure::class => 'lib/laravel/serializable-closure/src',
         \lbuchs\WebAuthn::class => 'lib/webauthn/src',
         \libphonenumber::class => 'lib/giggsey/libphonenumber-for-php-lite/src',
