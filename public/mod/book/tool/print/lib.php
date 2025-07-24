44f57a122fe796c5764482fe11511679da954902
Rajneel Totaram <rjnlfj@yahoo.com>
2025-07-24 12:20:47 +1200

    MDL-86095 mod_book: Let screenreader know print book opens in new window

--- public/mod/book/tool/print/lib.php
+++ public/mod/book/tool/print/lib.php
@@ -24,6 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die;
 
+use core\output\action_link;
+use core\output\html_writer;
+use core\url;
+
 /**
  * Adds module specific settings to the settings block
  *
@@ -32,20 +36,39 @@ defined('MOODLE_INTERNAL') || die;
  */
 function booktool_print_extend_settings_navigation(settings_navigation $settings, navigation_node $node) {
     $params = $settings->get_page()->url->params();
-    if (empty($params['id']) or empty($params['chapterid'])) {
+    if (empty($params['id']) || empty($params['chapterid'])) {
         return;
     }
 
     if (has_capability('booktool/print:print', $settings->get_page()->cm->context)) {
-        $url1 = new moodle_url('/mod/book/tool/print/index.php', array('id'=>$params['id']));
-        $url2 = new moodle_url('/mod/book/tool/print/index.php', array('id'=>$params['id'], 'chapterid'=>$params['chapterid']));
-        $action = new action_link($url1, get_string('printbook', 'booktool_print'), new popup_action('click', $url1));
-        $booknode = $node->add(get_string('printbook', 'booktool_print'), $action, navigation_node::TYPE_SETTING, null, 'printbook',
-                new pix_icon('book', '', 'booktool_print', array('class' => 'icon')));
+        $url1 = new url('/mod/book/tool/print/index.php', ['id' => $params['id']]);
+        $action1 = new action_link($url1, get_string('printbook', 'booktool_print'), new popup_action('click', $url1));
+        $text1 = get_string('printbook', 'booktool_print');
+        $text1 += html_writer::span('(' . get_string('opensinnewwindow') . ')', 'visually-hidden');
+
+        $booknode = $node->add(
+            $text1,
+            $action1,
+            navigation_node::TYPE_SETTING,
+            null,
+            'printbook',
+            new pix_icon('book', '', 'booktool_print', ['class' => 'icon'])
+        );
         $booknode->set_force_into_more_menu(true);
-        $action = new action_link($url2, get_string('printchapter', 'booktool_print'), new popup_action('click', $url2));
-        $chapternode = $node->add(get_string('printchapter', 'booktool_print'), $action, navigation_node::TYPE_SETTING, null,
-            'printchapter', new pix_icon('chapter', '', 'booktool_print', array('class' => 'icon')));
+
+        $url2 = new url('/mod/book/tool/print/index.php', ['id' => $params['id'], 'chapterid' => $params['chapterid']]);
+        $action2 = new action_link($url2, get_string('printchapter', 'booktool_print'), new popup_action('click', $url2));
+        $text2 = get_string('printchapter', 'booktool_print');
+        $text2 += html_writer::span('(' . get_string('opensinnewwindow') . ')', 'visually-hidden');
+
+        $chapternode = $node->add(
+            $text2,
+            $action2,
+            navigation_node::TYPE_SETTING,
+            null,
+            'printchapter',
+            new pix_icon('chapter', '', 'booktool_print', ['class' => 'icon'])
+        );
         $chapternode->set_force_into_more_menu(true);
     }
 }
@@ -60,5 +83,5 @@ function booktool_print_extend_settings_navigation(settings_navigation $settings
  * @return array
  */
 function booktool_print_get_view_actions() {
-    return array('print');
+    return ['print'];
 }
