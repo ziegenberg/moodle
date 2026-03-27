4da09fd5082b7d0c05331ebeed6fb6dc70fc55ae
Huong Nguyen <huongnv13@gmail.com>
2026-03-27 15:42:27 +0700

    Moodle release 5.2beta

--- public/version.php
+++ public/version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2026032000.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2026032700.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '5.2dev+ (Build: 20260320)';    // Human-friendly version name
+$release  = '5.2beta (Build: 20260327)';    // Human-friendly version name
 $branch   = '502';                      // This version's branch.
-$maturity = MATURITY_ALPHA;             // This version's maturity level.
+$maturity = MATURITY_BETA;              // This version's maturity level.
