9d88bc596c4024cd7dd804a6294b42f462e55307
Safat <safat.shahin@moodle.com>
2026-04-01 23:02:58 +1100

    MDL-88092 core_h5p: Fix version conflict

--- h5p/h5plib/v128/version.php
+++ h5p/h5plib/v128/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026031100;       // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2025041401;       // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2025040800;       // Requires this Moodle version.
 $plugin->component = 'h5plib_v128';    // Full name of the plugin (used for diagnostics).
