b47c5a2008b1d7c6b8b10f585542ace77320c793
Mihail Geshoski <mihail@moodle.com>
2026-04-15 18:51:19 +0800

    Moodle release 5.2rc2

--- public/version.php
+++ public/version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2026041500.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2026041500.01;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '5.2rc1 (Build: 20260410)';    // Human-friendly version name
+$release  = '5.2rc2 (Build: 20260415)';    // Human-friendly version name
 $branch   = '502';                      // This version's branch.
 $maturity = MATURITY_RC;                // This version's maturity level.
