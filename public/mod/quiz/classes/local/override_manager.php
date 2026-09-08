250d590c256568f443972619d9b0164a513dd321
Matt Porritt <matt.porritt@moodle.com>
2026-09-08 15:03:46 +1000

    MDL-89711 mod_quiz: point todo at final deprecation follow-up
    Per peer review, reference the newly created follow-up task
    MDL-89747 (child of MDL-78075) in the @todo tag instead of
    self-referencing this issue.

--- public/mod/quiz/classes/local/override_manager.php
+++ public/mod/quiz/classes/local/override_manager.php
@@ -642,7 +642,7 @@ class override_manager {
      * Computes the effective overridden open and close times for a user for a given quiz.
      *
      * @deprecated Since Moodle 5.3. Use override_manager::get_effective_times() instead.
-     * @todo Final deprecation in Moodle 6.0. See MDL-89711.
+     * @todo Final deprecation in Moodle 7.0. See MDL-89747.
      * @param int $quizid The quiz ID.
      * @param int $userid The user ID.
      * @return array Array with optional keys 'timeopen' and 'timeclose'.
